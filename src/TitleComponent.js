42bcdb4f6b00f1f47ec07a2d0fcf105c13664940
nkukolj16 <knikola97@gmail.com>
2023-01-25 15:31:39 +0100

    css fix

--- src/TitleComponent.js
+++ src/TitleComponent.js
@@ -4,7 +4,9 @@ import { useSelector } from "react-redux";
 const TitleComponent = () => {
   const titleColor = useSelector((state) => state.testReducer.titleColor) || "black";
   return (
-    <h2 className="mainTitle" style={{color: titleColor}}>Let's get started!</h2>
+    <div className="container">
+      <h2 className="mainTitle" style={{color: titleColor}}>Let's get started!</h2>
+    </div>
   );
 };
 export default TitleComponent;
